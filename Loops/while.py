e5ad85f8bfd1148a6e8d4dcf245c03a847e11bc0
phenol2002 <86253756+prerana30@users.noreply.github.com>
2023-04-17 07:06:25 +0545

    Revision

---
+++ Loops/while.py
@@ -0,0 +1,17 @@
+# I want to print Hello everyone , everyone four times and again in next line Hello and everyone four times so I will be using a loop.
+
+i=1
+while i<=5:
+    print("Hello",end="")
+    j=1
+
+    while j<=4:
+        print("everyone",end="")
+        j=j+1
+    
+ 
+    i=i+1
+    print()
+
+     
+ 
